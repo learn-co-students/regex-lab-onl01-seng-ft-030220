5e2613bad6ca32e4637f688bab2d28f5a09a3f05
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-28 21:20:06 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,39 @@
+require 'pry'
 def starts_with_a_vowel?(word)
+  #It matches the beginning of the string \A followed by a vowel [aeiou] in a case-insensitive manner i returning a bool word.match?
+  if word.match(/\A[aeiou]/i)
+    true 
+  else
+    false 
+  end 
+end 
 
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b[a-zA-Z]{5}\b/)#/b Matches a word-break.
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
+  if text.match(/^[A-Z].+[.,"':;!?\/-]$/)
+    true
+  else
+    false 
+  end 
+  
 end
 
 def valid_phone_number?(phone)
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true 
+  else 
+    false 
+  end 
 
 end
